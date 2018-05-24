0729b7ac7ee81a195d10a2c3d275cde23e8007a5
ShinsukeArai <shinsuke.r.arai@gmail.com>
2018-05-24 16:42:48 +0900

    Elastic APIの記入

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '<用意したElastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '13.231.34.145', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
