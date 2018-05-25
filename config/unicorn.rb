8d6ea19fb7f9050458638b12736136d961fdf353
ShinsukeArai <shinsuke.r.arai@gmail.com>
2018-05-25 19:56:00 +0900

    modify unicorn_socket_at

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 app_path = File.expand_path('../../../', __FILE__)
 working_directory "#{app_path}/current"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
